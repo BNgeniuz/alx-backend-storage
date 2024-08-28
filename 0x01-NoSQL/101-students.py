2dcf0ba6d5f6d7d53e4fe0d7be90afa5d17b37af
Godwin Bruce <godwin.bruce@gmail.com>
2024-08-27 17:13:15 -0700

    studata

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -2,17 +2,27 @@
 
 """
 Function to return all students sorted by average score
-"""def top_students(mongo_collection):
-    # sourcery skip: inline-immediately-returned-variable
-    """ Returns all students sorted by average score """
-    top_student = mongo_collection.aggregate([
-        {
-            "$project": {
-                "name": "$name",
-                "averageScore": {"$avg": "$topics.score"}
-            }
-        },
-        {"$sort": {"averageScore": -1}}
-    ])
-
-    return top_student
+"""
+def top_students(mongo_collection):
+    '''Prints all students in a collection sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
