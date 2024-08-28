9eb2f1a13d5066cd43b6d2d5dec4d84a538d6831
Godwin Bruce <godwin.bruce@gmail.com>
2024-08-27 17:09:45 -0700

    studata

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -2,36 +2,17 @@
 
 """
 Function to return all students sorted by average score
-"""
-
-from pymongo import MongoClient
-
-def top_students(mongo_collection):
-    """
-    Returns all students sorted by average score.
-    
-    Parameters:
-    - mongo_collection: the pymongo collection object
-
-    Returns:
-    - List of students sorted by average score with key = averageScore
-    """
-    pipeline = [
+"""def top_students(mongo_collection):
+    # sourcery skip: inline-immediately-returned-variable
+    """ Returns all students sorted by average score """
+    top_student = mongo_collection.aggregate([
         {
             "$project": {
-                "name": 1,
-                "averageScore": {"$avg": "$scores.score"}
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
             }
         },
-        {
-            "$sort": {"averageScore": -1}
-        }
-    ]
-    return list(mongo_collection.aggregate(pipeline))
+        {"$sort": {"averageScore": -1}}
+    ])
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.school
-    collection = db.students
-    for student in top_students(collection):
-        print(student)
+    return top_student
